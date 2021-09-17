20b325fe6b8b5c6816645f0400bb515c60bdd34f
user <user@DESKTOP-S6ON2JF>
2021-09-17 12:07:26 +0900

    ㅁ

--- ch02/src/ch08/VariableScopeExam2.java
+++ ch02/src/ch08/VariableScopeExam2.java
@@ -1,30 +1,24 @@
 package ch08;
 
-public class VariableScopeExam1 {
+public class VariableScopeExam2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
       //main메소드 블럭내에서 선언된 로컬변수
-	  int var1=0;
+	  int var1=0;// 선언 후 값 초기화
       
       if(true) {
     	  //main메소드내의 if블럭에서 선언된 로컬변수
     	  int var2;
-    	  System.out.println(var1);
-    	  var1 = 1;// main메소드 내의 if블럭내에서 사용가능.
-    	  var2 = 2;//자신이 선언된 블럭내에서 사용가능
-    	  int result = var1 + var2;
-    	  System.out.println(result);
-    	  //int var1=10;
+          int var3;    	  
+    	  var1=1; //값 변경
+    	  //상위 범위의 블럭내에서 선언된 변수를 하위 범위 블럭에서 선언시 중복선언오류발생
+    	  //int var1;
       }
       while(true) {
     	  //main메소드내의 while블럭에서 선언된 로컬변수
     	  int var3;
-    	  System.out.println(var1);//main에서 선언되고 if블럭에서 값이 바뀐 변수
-    	  var3=3;
-    	  var1=4;//while블럭에서 새로운 값 저장 
-    	  System.out.println(var1+var3);
-    	  //
-    	 //  var2=5;
+    	  int var2;
+    	  
     	  break;//반복문을 빠져나가게 하는 명령어
       }
       System.out.println(var1);
