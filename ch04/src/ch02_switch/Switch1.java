bcc6ea1ab6b80e3d7ea7fdb0d1dd1b0541916ba8
user <user@DESKTOP-S6ON2JF>
2021-09-24 16:08:02 +0900

    ㅁ

--- ch04/src/ch02_switch/Switch1.java
+++ ch04/src/ch02_switch/Switch1.java
@@ -7,7 +7,7 @@ public class Switch1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 	Scanner scanner = new Scanner(System.in);
-	 System.out.println("숫자를 입력하세요:");
+	
       int num = scanner.nextInt();
       switch(num) {
       case 1: System.out.print("L");break;
