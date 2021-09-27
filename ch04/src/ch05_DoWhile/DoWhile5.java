e82c692a080edb326d0fc4b7095df8a3ef366e87
user <user@DESKTOP-S6ON2JF>
2021-09-27 12:49:24 +0900

    ㅁ

---
+++ ch04/src/ch05_DoWhile/DoWhile5.java
@@ -0,0 +1,26 @@
+package ch05_DoWhile;
+
+import java.util.Scanner;
+
+public class DoWhile5 {
+//
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      Scanner scanner = new Scanner(System.in);
+      int ran = (int)(Math.random()*100)+1;//정수 임의값 저장
+      int cnt =1;
+      do {
+    	  System.out.println("1부터100까지의 숫자 입력!");
+    	  int num = scanner.nextInt();
+    	  if(num ==ran) {
+    		  System.out.println("축하!"+cnt+"번 만에 맞췄습니다.");
+    		  break;
+    	  }else if(num > ran) {
+    		  System.out.println("입력된숫자가 랜텀숫자보다 큽니다.");
+    	  }else {
+    		  System.out.println("입력된숫자가 랜텀숫자보다 작습니다.");
+    	  }
+    	  cnt++;
+      }while(true);//무한 반복
+	}
+}
