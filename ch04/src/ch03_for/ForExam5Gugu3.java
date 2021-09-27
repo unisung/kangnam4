3388d0ed0f30d8438458c1e69a06945e66b0e06c
user <user@DESKTOP-S6ON2JF>
2021-09-27 10:21:09 +0900

    a

--- ch04/src/ch03_for/ForExam5Gugu3.java
+++ ch04/src/ch03_for/ForExam5Gugu3.java
@@ -1,12 +1,12 @@
 package ch03_for;
 
-public class ForExam5Gugu2 {
+public class ForExam5Gugu3 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 	 int i;
-	 for(i=2;i<=9;i++) {
-		 for(int j=1;j<=9;j++) {
-		   System.out.print(i+"*" +j+"="+i*j+"\t");
+	 for(i=1;i<=9;i++) {
+		 for(int j=2;j<=9;j++) {
+		   System.out.print(j+"*" +i+"="+i*j+"\t");
 		 }
 		 System.out.println();//한줄 밑으로 내리는 명령문;
 	 }
