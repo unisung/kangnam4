6a22b69099f1c6131df93ed56c2f8effc965262b
user <user@DESKTOP-S6ON2JF>
2021-09-27 10:48:07 +0900

    ㅁ

---
+++ ch04/src/ch04_while/While4.java
@@ -0,0 +1,23 @@
+package ch04_while;
+
+import java.io.IOException;
+
+public class While4 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("2~9사이의 숫자를 입력하세요.");
+		//키보드로 숫자 0을 누르면 입력되는 값은? 48 <-ASCII코드 값
+		try {
+			int num = System.in.read()-'0';//표준입력 스트림 '0'키를 누른경우:48-48,'1'=>49-48=1
+			System.out.println("구구단 "+num +"단");
+			int i=1;
+			while(i<=9) {// while블럭{}을 빠져나가는 조건
+				System.out.println(num+"*"+i+"="+(i*num));//출력
+				i++;//증가 명령문
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			System.out.println("오류발생");
+		}
+	}
+}
