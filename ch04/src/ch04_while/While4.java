7160a55b641204b50f7e8f285a58bd06d3ae508d
user <user@DESKTOP-S6ON2JF>
2021-09-27 11:09:14 +0900

    ㅁ

--- ch04/src/ch04_while/While4.java
+++ ch04/src/ch04_while/While4.java
@@ -10,10 +10,14 @@ public class While4 {
 		try {
 			int num = System.in.read()-'0';//표준입력 스트림 '0'키를 누른경우:48-48,'1'=>49-48=1
 			System.out.println("구구단 "+num +"단");
-			int i=1;
-			while(i<=9){// while블럭{}을 빠져나가는 조건
-				System.out.println(num+"*"+i+"="+(i*num));//출력
-				i++;//증가 명령문
+			if(num>=2 && num <=9) {
+				int i=1;
+				while(i<=9){// while블럭{}을 빠져나가는 조건
+					System.out.println(num+"*"+i+"="+(i*num));//출력
+					i++;//증가 명령문
+				 }
+			}else {
+				System.out.println("2~9까지의 숫자만 입력하세요!!!");
 			}
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
