620facd231463cea9d44925e9d51a3b225138f85
user <user@DESKTOP-S6ON2JF>
2021-09-27 09:48:10 +0900

    1

---
+++ ch04/src/ch03_for/ForExam5Gugu2.java
@@ -0,0 +1,15 @@
+package ch03_for;
+
+public class ForExam5Gugu2 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	 int i;
+	 for(i=1;i<=9;i++) {
+		 for(int j=1;j<=9;j++) {
+		   System.out.print(i+"*" +j+"="+i*j+"\t");
+		 }
+		 System.out.println();//한줄 밑으로 내리는 명령문;
+	 }
+	 System.out.println("i="+i);//? 
+	}
+}
