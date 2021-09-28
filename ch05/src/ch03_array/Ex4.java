9630e38de9e477c94e0636598265f541dc10a873
user <user@DESKTOP-S6ON2JF>
2021-09-28 14:28:22 +0900

    a

--- ch05/src/ch03_array/Ex4.java
+++ ch05/src/ch03_array/Ex4.java
@@ -6,6 +6,14 @@ public class Ex4 {
     int[] score= {78,98,54,76,87};
     System.out.println("¹è¿­ÀÇ °¹¼ö:"+score.length);
 	//ÃÑÁ¡
-    //Æò±Õ
+    int total=0;
+    for(int i=0;i<score.length;i++) {
+    	total+=score[i];
+    }
+    System.out.println("ÃÑÁ¡:"+total);
+    //Æò±Õ-doubleÅ¸ÀÔ ÇüÅÂÃâ·Â
+    double avg=0.0;
+    avg=total/(double)score.length;
+    System.out.println("Æò±Õ:"+avg);
 	} 
 }
