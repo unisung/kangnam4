503a5f44a115fb2353fd0ee7891bf337454bc8df
user <user@DESKTOP-S6ON2JF>
2021-09-29 17:22:51 +0900

    1

---
+++ ch05/src/work/Work01.java
@@ -0,0 +1,35 @@
+package work;
+/*과제 제출기한 10/4일(월) 23:59분 까지
+ * 스캐너로 입력값 입력받기 성별, 나이, 키, 몸무게
+ * 반복처리 가능하게 만들고 
+ * 계속 여부 출력후 yes/no값을 받아서 종료하도록 수정하기.
+ * */
+public class Work01 {
+	public static void main(String[] args) {
+		
+		// 제지방량 구하기
+		final double ST1 = 1.10;
+		final double ST2 = 128;
+
+		int weight = 75;
+		int height = 175;
+
+		//제지방량
+		double result = ST1 * weight - ST2 * 
+				        ((weight * weight) /(double)(height * height)); 
+
+		System.out.println(result);
+
+		// 체지방량 구하기
+		double result2 = weight - result;
+
+		System.out.println(result2);
+
+		// 체지방률 구하기 = 체지방량*100/체중
+		final double ST3 = 100;
+
+		double result3 = result2 * ST3 / weight;
+
+		System.out.println(result3);
+	}
+}
