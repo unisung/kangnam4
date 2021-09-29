164a73d4ebd053f5578cd68908a8efb322fdc12b
user <user@DESKTOP-S6ON2JF>
2021-09-29 17:43:35 +0900

    a

--- ch05/src/ch04_array2/Exam01.java
+++ ch05/src/ch04_array2/Exam01.java
@@ -1,5 +1,7 @@
 package ch04_array2;
 
+import java.util.Scanner;
+
 public class Exam01 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -7,14 +9,23 @@ public class Exam01 {
 	  // id와 pwd를 scanner로 입력받아서 id와 pwd가 둘다 같으면 "login성공",
 	  // 아니면 "1회 실패하였습니다". 출력
 	  // 5회 실패하면, "5회 실패하였습니다. 프로그램을 종료 합니다" 출력 후 종료
+	  Scanner scanner = new Scanner(System.in);
+	  int cnt=0;
 	  while(true) {
-		   if(cnt==5)System.out.println(cnt+"회 실패하였습니다. 프로그램을 종료 합니다");
+		   String id,pwd;
+		   if(cnt==5) {//기본데이타 타입의 값 비교
+			   System.out.println(cnt+"회 실패하였습니다. 프로그램을 종료 합니다");
+			   break;
+		   }
 		   System.out.println("아이디와 패스워드를 입력하세요.");
 		   System.out.println("아이디>");
+		   id=scanner.next();
 		   System.out.println("패스워드>");
-		   if(id가"hong"이고 pwd가"1234") {"로그인 성공"; break;}
+		   pwd=scanner.next();
+		   if("hong".equals(id) && "1234".equals(pwd))//참조객체의 비교 와 논리곱연산 
+		        {"로그인 성공"; break;}
 		   //실패시
-		   System.out.println(cnt+"회 실패하였습니다.");
+		   System.out.println(cnt++ +"회 실패하였습니다.");
 	  }
 		System.out.println("프로그램 종료");
 	}
