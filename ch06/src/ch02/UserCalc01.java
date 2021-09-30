d68d5d0e6380b3ef2479de3cecbb51cc3c32f206
user <user@DESKTOP-S6ON2JF>
2021-09-30 17:59:00 +0900

    a

--- ch06/src/ch02/UserCalc01.java
+++ ch06/src/ch02/UserCalc01.java
@@ -5,7 +5,8 @@ import java.util.Scanner;
 public class UserCalc01 {
 	public static void main(String[] args) {
 	 Scanner scanner = new Scanner(System.in);
-	 
+	 boolean run=true;
+	while(run) { 
 	 System.out.println("두개의 정수를 입력하세요.");
 	 System.out.println("첫 번째 정수>");
 	 int x=scanner.nextInt();
@@ -17,6 +18,11 @@ public class UserCalc01 {
 	 int result2=cal.sub(x, y);
 	 System.out.println(result1);
 	 System.out.println(result2);
-		
+	 
+	 System.out.println("계속할까요?(yes/no)");
+	 String yesNo = scanner.next();
+	 if("No".equals(yesNo)) run=!run;
+	  }
+	System.out.println("종료");
 	}
 }
