f84e4b062068076a26123ef772c5c1474aa35c77
user <user@DESKTOP-S6ON2JF>
2021-09-30 16:32:20 +0900

    a

--- ch06/src/ch02/Calculator.java
+++ ch06/src/ch02/Calculator.java
@@ -2,8 +2,11 @@ package ch02;
 
 //public 키워드는 외부에서 접근가능한 접근 제어자임.
 public class Calculator {
-	public String model="00001";
-    public double result=0;
+	//model명은 외부에서 접근가능, 수정되면 안됨.
+	public final String model="00001";
+	//result값은 getCalc에 의해서 처리된 값만 받음.외부에서 접근불가
+    private double result=0;
+    //같은 클래스내의 메소드들끼리는 상호 호출 가능
   public void getCalc(String op, int x, int y) {
 		switch(op) {
 		case "+": result =add(x,y);
@@ -16,8 +19,9 @@ public class Calculator {
 			      System.out.println(result); break;
 		}
 	}
-   public int add(int x, int y) { return x+y; }
-   public int sub(int x, int y) { return x-y; }
-   public int mul(int x, int y) { return x*y; }
-   public double div(int x, int y) { return x/(double)y; }
+   //아래의 메소들은 getCalc()메소드만 호출가능-외부에서 호출불가
+   private int add(int x, int y) { return x+y; }
+   private int sub(int x, int y) { return x-y; }
+   private int mul(int x, int y) { return x*y; }
+   private double div(int x, int y) { return x/(double)y; }
 }
---
+++ ch06/src/ch02/UseCalcExam.java
@@ -0,0 +1,21 @@
+package ch02;
+
+public class UseCalcExam {
+ public static void main(String[] args) {
+	 //객체 생성
+	 Calculator cal = new Calculator();
+	 cal.getCalc("+", 10, 20);
+	 //외부에서 접근 가능
+	 System.out.println(cal.model);
+	 //외부에서 접근불가
+	 //System.out.println(cal.result);
+	 //외부에서 접근불가
+	  //int res = cal.sub(10,20);
+	 //cal.result=cal.sub(10, 20);
+	 //System.out.println(cal.result);
+	 //외부에서 수정 불가
+	 // cal.model="99999";
+	 System.out.println(cal.model);
+	
+}
+}
