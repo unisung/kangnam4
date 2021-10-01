d2ec142c0137eff1dad326d9a85e7580d487011c
user <user@DESKTOP-S6ON2JF>
2021-10-01 12:49:47 +0900

    ㅁ

---
+++ ch06/src/ch05_constructor_01/UserCar.java
@@ -0,0 +1,12 @@
+package ch05_constructor_01;
+
+public class UserCar {
+public static void main(String[] args) {
+  //컴파일된 클래스에 Car()기본생성자가 없음.
+  //호출불가 이므로 오류 발생.	
+  //Car car = new Car();
+  //다른 생성자 - 클래스에 있는 생성자 호출
+Car car = new Car("소나타","흰색",200);
+
+	}
+}
