a9674bd8d9fcac3716d9ebf10ba64994ea45a265
user <user@DESKTOP-S6ON2JF>
2021-10-01 12:33:05 +0900

    a

---
+++ ch06/src/ch05_constructor/Car.java
@@ -0,0 +1,15 @@
+package ch05_constructor;
+
+//소스코드
+public class Car{}
+/*
+ * 생성자- 
+ * 클래스의 접근제한자 class 클래스명(){}
+ * 
+ *소스코드에 기본 생성자가 없으면
+ *컴파일러가 컴파일시 기본생성자 추가
+ * 컴파일 후 -
+ * public class Car{
+ *  public class Car(){}
+ *  }
+ */
---
+++ ch06/src/ch05_constructor/UserCar.java
@@ -0,0 +1,10 @@
+package ch05_constructor;
+
+public class UserCar {
+public static void main(String[] args) {
+	//Car객체 생성
+	//new Car(); 호출
+     Car car = new Car();
+//소스코드로 부터 컴파일된 클래스Car클래스파일로 부터 객체 생성
+	}
+}
