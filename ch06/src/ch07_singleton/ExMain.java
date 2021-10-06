e37c5a470864b4e230a96dbeae04e1a44f10a43a
user <user@DESKTOP-S6ON2JF>
2021-10-06 11:24:08 +0900

    1

---
+++ ch06/src/ch07_singleton/ExMain.java
@@ -0,0 +1,15 @@
+package ch07_singleton;
+
+public class ExMain {
+	public static void main(String[] args) {
+	//multiton방식 - new 생성자();
+	 //SingleTon s1 = new SingleTon();
+     //SingleTon s1 = SingleTon.instance;
+	 //클래스명.static메소드호출()
+      SingleTon s1 = SingleTon.getInstance();
+      SingleTon s2 = SingleTon.getInstance();
+     
+      System.out.println(s1==s2?"같은객체":"다른객체");
+      
+	}
+}
---
+++ ch06/src/ch07_singleton_01/SingleTon.java
@@ -0,0 +1,15 @@
+package ch07_singleton_01;
+
+/*싱글톤으로 클래스 선언 */
+public class SingleTon {
+//1.자신타입의 static필드 선언
+   private static SingleTon instance;
+//2.생성자를 외부에서 접근 못하게 private로 처리
+   private SingleTon(){}
+//3.public 메소드로 접근하도록 메소드 선언   
+public static SingleTon getInstance() {
+	if(instance==null) instance=new SingleTon();
+	return instance;
+}
+
+}
