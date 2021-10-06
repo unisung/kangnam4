b92a29b77d441e0435834d5a3d9749161c4cf152
user <user@DESKTOP-S6ON2JF>
2021-10-06 14:52:34 +0900

    a

---
+++ ch06/src/ch08/Car.java
@@ -0,0 +1,25 @@
+package ch08;
+
+public class Car {
+	//필드
+	private int speed;
+	private boolean stop;
+	
+	//생성자
+	//메소드
+	public int getSpeed() {
+		return speed;
+	}
+	
+	public void setSpeed(int speed) {
+		 this.speed=speed;
+	}
+	
+	public boolean isStop() {
+		return stop;
+	}
+	
+	public void setStop(boolean stop) {
+		this.stop=stop;
+	}
+}
---
+++ ch06/src/ch08/Car2.java
@@ -0,0 +1,26 @@
+package ch08;
+
+public class Car2 {
+	//ÇÊµå
+	private int speed;
+	private boolean stop;
+	
+	public int getSpeed() {
+		return speed;
+	}
+	public void setSpeed(int speed) {
+		if(speed<0) {
+			this.speed = 0;
+			return;
+		}else {
+			this.speed = speed;			
+		}
+	}
+	public boolean isStop() {
+		return stop;
+	}
+	public void setStop(boolean stop) {
+		this.stop = stop;
+		this.speed=0;
+	}
+}
