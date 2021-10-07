19291c389a7bf15b50333ed2fa6b2c8b14123694
user <user@DESKTOP-S6ON2JF>
2021-10-07 12:06:03 +0900

    ㅁ

--- ch06/src/verify/Account.java
+++ ch06/src/verify/Account.java
@@ -6,12 +6,23 @@ import java.util.Arrays;
 
 public class Account {
 	//필드
+ static final String bankNo="111";
+ static int seq=0;
  private String ano;
  private String owner;
  private int balance;
+
+ {
+	seq++;
+	ano = bankNo +"-"+seq+seq+seq;
+ }
  //생성자
 public Account(String ano, String owner, int balance) {
 	this.ano = ano;
+	this.owner = owner;
+	this.balance = balance;
+}
+public Account(String owner, int balance) {
 	this.owner = owner;
 	this.balance = balance;
 }
