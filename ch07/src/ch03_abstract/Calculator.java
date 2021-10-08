dc1974d4f8bf5f79f39a2ad03f576c62ca30f632
user <user@DESKTOP-S6ON2JF>
2021-10-08 16:00:21 +0900

    a

--- ch07/src/ch03_abstract/Calculator.java
+++ ch07/src/ch03_abstract/Calculator.java
@@ -7,6 +7,7 @@ public abstract class Calculator {
 }
 
 class Calc extends Calculator{
+
 	@Override
 	public int add(int a, int b) {
 		return a+b;
@@ -20,9 +21,9 @@ class Calc extends Calculator{
 	@Override
 	public double average(int[] a) {
 		int sum=0;
-        for(int i=0;i<a.length;i++) {
-        	 sum+=a[i];
-        }
+       for(int i:a) {
+    	   sum+=i;
+       }
 		return sum/a.length;
 	}
 }
