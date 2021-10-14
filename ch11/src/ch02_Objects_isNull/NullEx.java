a3c6e7e8f708ea7d7c43fa0393335e0088d111a5
user <user@DESKTOP-S6ON2JF>
2021-10-14 14:33:13 +0900

    ㅁ

--- ch11/src/ch02_Objects_isNull/NullEx.java
+++ ch11/src/ch02_Objects_isNull/NullEx.java
@@ -13,5 +13,15 @@ public static void main(String[] args) {
 	//null여부확인
 	System.out.println(Objects.isNull(s1));
 	System.out.println(Objects.isNull(s2));
+   
+	//메세지 출력
+	//System.out.println(Objects.requireNonNull(s2,"이름이 없음."));
+	
+	//toString()
+	System.out.println(Objects.toString(s1));
+	System.out.println(Objects.toString(s2));//"null"
+	System.out.println(Objects.toString(s2,"이름이 없습니다."));//지정한 메세지
+
 }
+  
 }
