c14014c1d647ebee079c5a3e9c4653b2a770f763
user <user@DESKTOP-S6ON2JF>
2021-10-15 10:12:52 +0900

    ㅁ

---
+++ ch11/src/ch02_String02/StringBuilderEx01.java
@@ -0,0 +1,37 @@
+package ch02_String02;
+
+public class StringBuilderEx01 {
+	public static void main(String[] args) {
+		//
+	 StringBuilder sb = new StringBuilder();
+	 //문자열 이어붙익 append()
+	 sb.append("Java ");
+	 sb.append("Programming study");
+	 //
+	 System.out.println(sb);//toString()내용출력
+	 
+	 //삽입 insert(index,문자열)
+	 sb.insert(4, "2");
+	 System.out.println(sb);//toString()내용출력
+	 //setCharAt(index,바꿀문자)
+	 sb.setCharAt(4, '6');
+	 System.out.println(sb);//toString()내용출력
+	 
+	 //replace(시작index,끝index,바꿀문자열);  시작index번호<=범위<끝index
+	 sb.replace(6, 13, "Book");
+	 System.out.println(sb);//toString()내용출력
+	 
+	 // delete(시작index,끝index); 시작index <=범위 < 끝index
+	 sb.delete(4,5);
+	 System.out.println(sb);//toString()내용출력
+	 
+	 int length = sb.length();
+	 System.out.println("총 문자수:"+length);
+	 
+	 //
+    String result = sb.toString();//버퍼내용을 추출
+    System.out.println(result);
+	 
+	}
+
+}
