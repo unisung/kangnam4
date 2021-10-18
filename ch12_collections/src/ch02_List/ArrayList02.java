09ac985fc1a84136f05c639d2154a99dd54c7d32
user <user@DESKTOP-S6ON2JF>
2021-10-18 11:37:27 +0900

    ㅁ

--- ch12_collections/src/ch02_List/ArrayList02.java
+++ ch12_collections/src/ch02_List/ArrayList02.java
@@ -7,6 +7,7 @@ public class ArrayList02 {
 
 	public static void main(String[] args) {
 		//1.String객체를 담는 List만들고 
+		//ArrayList<String> list=new ArrayList<>();
 		List<String> list=new ArrayList<>();
 		//2."홍길동", "일지매", "임꺽정" 을 저장 후
 		list.add("홍길동");list.add("일지매");list.add("임꺽정");
@@ -14,13 +15,20 @@ public class ArrayList02 {
 		System.out.println(list.size());
 		//4. "이순신"이 들어있는지 확인
 		System.out.println(list.contains("이순신")?"들어있음":"없음");
-		//5. 모든 객체를 출력
+		//5. 모든 객체를 출력 [a][a][null][a][null][a]
+		//[a][a][a][a]
 		for(int i=0;i<list.size();i++)
 			System.out.println(list.get(i));
-		for(String s:list)
+		//["hello"]["hi"]["bye"]
+		for(String s:list)//s <- 반복자 
 			System.out.println(s);
+		System.out.println();
 		//6. index번호 2번에 해당하는 객체를 "유관순"으로 변경하세요.
 		list.set(2, "유관순");
+		for(String s:list)
+			System.out.println(s);
+		
+		System.out.println("-----------------------");
 		//7. index번호 2번에 해당하는 객체를 출력하세요
 		System.out.println(list.get(2));
 		//8. 리스트내의 모든 객체를 삭제후  
