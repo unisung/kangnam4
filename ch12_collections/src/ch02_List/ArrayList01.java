c72b9a70c062bcb41786b94b059f01b6dfd1f74f
user <user@DESKTOP-S6ON2JF>
2021-10-18 11:11:43 +0900

    ㅁ

--- ch12_collections/src/ch02_List/ArrayList01.java
+++ ch12_collections/src/ch02_List/ArrayList01.java
@@ -14,29 +14,36 @@ public static void main(String[] args) {
 	
 	//객체의 갯수 확인 size()
 	int n = list.size();
-	//int c = list.capacity(); -없음
+	//int c = list.capacity(); -메소드가 없음
 	System.out.println(n);
 	
 	//중간 삽입 add(index,객체)
 	list.add(2, "Shani");
 	//list.add(5,"Shani");//java.lang.IndexOutOfBoundsException: Index: 5, Size: 4
 	
+	//객체 저장여부확인 contains(객체)
+    System.out.println(list.contains("Java")?"Java문장이 있습니다.":"Java문장이 없습니다.");	
+	
 	//저장된 객체 얻기 get(index)
 	String str = list.get(1);
 	System.out.println(str);
+	
+	//저장된 객체를 Object배열로 리턴
+	Object[] strArr = list.toArray();
+	for(Object s:strArr)
+		System.out.print(s+" ");
+	
+	System.out.println();
 	
 	//삭제 remove(index번호)
 	list.remove(1);
 	//size()값보다 큰 index번호로 삭제시 오류 발생
 	//list.remove(4);//java.lang.IndexOutOfBoundsException: Index: 4, Size: 3
 	
+	//모두 삭제
+	list.clear();
 	
-	
-	
-	
-	
-	
-	
+	System.out.println(list.isEmpty()?"리스트가 비어있습니다.":"리스트가 비어있지 않습니다.");
 	
 	
 	
