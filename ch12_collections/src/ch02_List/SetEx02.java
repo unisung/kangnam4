ba35bc382031844fdeea9e593ec21440c5c8c7c5
user <user@DESKTOP-S6ON2JF>
2021-10-18 15:52:17 +0900

    a

--- ch12_collections/src/ch02_List/SetEx02.java
+++ ch12_collections/src/ch02_List/SetEx02.java
@@ -1,11 +1,29 @@
 package ch02_List;
 
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Objects;
+import java.util.Set;
+
 public class SetEx02 {
 public static void main(String[] args) {
    //Set인터페이스 로 선언된 set생성
    //Member1 String sno, String name으로 구성
    //"1","홍길동"  "2","일지매"  "3","임꺽정", "2","일지매"
    //중복배제하여 저장 후 모든 객체를 출력
+	Set<Member1> set = new HashSet<>();
+	set.add(new Member1("1", "홍길동"));
+	set.add(new Member1("2", "일지매"));
+	set.add(new Member1("3", "임꺽정"));
+	set.add(new Member1("2", "일지매"));
+	
+	//출력
+	Iterator<Member1> itor = set.iterator();
+	while(itor.hasNext()) {
+		Member1 m=itor.next();
+		System.out.println(m);
+	}
+	
 	 
 }
 }
@@ -27,6 +45,25 @@ class Member1{
 	public String toString() {
 		return "Member1 [sno=" + sno + ", name=" + name + "]";
 	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(name, sno);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Member1 other = (Member1) obj;
+		return Objects.equals(name, other.name) && Objects.equals(sno, other.sno);
+	}
+	
+	
+
 	
 	
 }
