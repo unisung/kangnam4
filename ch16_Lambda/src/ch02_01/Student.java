7df4a2c45916fa2a7e0baf9881f8b228317dd5f4
user <user@DESKTOP-S6ON2JF>
2021-10-21 15:26:00 +0900

    a

---
+++ ch16_Lambda/src/ch02_01/Student.java
@@ -0,0 +1,25 @@
+package ch02_01;
+
+public class Student {
+ private String name;
+ private String gender;
+ private int score;
+ 
+public Student(String name, String gender, int score) {
+	this.name = name;
+	this.gender = gender;
+	this.score = score;
+}
+
+public String getGender() {
+	return gender;
+}
+
+public int getScore() {
+	return score;
+}
+
+
+ 
+ 
+}
