3a2f77bd7cd955b356c20847078f4b23cfc8142e
user <user@DESKTOP-S6ON2JF>
2021-10-21 14:57:38 +0900

    a

---
+++ ch16_Lambda/src/ch02/FunctionExample.java
@@ -0,0 +1,60 @@
+package ch02;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+import java.util.function.ToIntFunction;
+
+
+public class FunctionExample {
+	private static List<Student> list 
+	        =Arrays.asList(new Student("홍길동",90,96),new Student("일지매",95,93));
+	
+	public static void printString(Function<Student,String> function) {
+		for(Student student:list) {
+			System.out.println(function.apply(student)+" ");
+		}
+		System.out.println();
+	}
+	
+	public static void printInt(ToIntFunction<Student> function) {
+		for(Student student:list) {
+			System.out.println(function.applyAsInt(student)+" ");
+		}
+		System.out.println();
+	}
+
+	public static void main(String[] args) {
+	 System.out.println("[학생 이름]");
+	 printString(new Function<Student, String>() {
+		@Override
+		public String apply(Student student) {
+			return student.getName();
+		}
+	});
+	 //람다식
+	 printString(student->student.getName());
+	 
+    System.out.println("[영어점수]");
+    printInt(new ToIntFunction<Student>() {	
+		@Override
+		public int applyAsInt(Student value) {
+			return value.getEnglishScore();
+		}
+	});
+    //람다식
+    printInt(value->value.getEnglishScore());
+    
+    System.out.println("[수학점수]");
+    printInt(new ToIntFunction<Student>() {
+		@Override
+		public int applyAsInt(Student student) {
+			return student.getMathScore();
+		}
+	});
+    //람다식
+    printInt(student->student.getMathScore());
+    
+	}
+
+}
---
+++ ch16_Lambda/src/ch02/FunctionExample2.java
@@ -0,0 +1,32 @@
+package ch02;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.ToIntFunction;
+
+public class FunctionExample2 {
+	private static List<Student> list 
+    =Arrays.asList(new Student("홍길동",90,96),new Student("일지매",95,93));
+	
+	public static double avg(ToIntFunction<Student> function) {
+		int sum=0;
+		for(Student student:list) {
+			sum+=function.applyAsInt(student);
+		}
+		double avg =(double)sum/list.size();
+		return avg;
+	}
+	
+	public static void main(String[] args) {
+		double englishAvg = avg(new ToIntFunction<Student>() {
+			@Override
+			public int applyAsInt(Student value) {
+				return value.getEnglishScore();
+			}
+		});
+		System.out.println(englishAvg);
+		//람다식
+		englishAvg=avg(value->value.getEnglishScore());
+		System.out.println(englishAvg);
+	}
+}
