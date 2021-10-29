c57897d84f025d2ab5dd3d6770ac32de1fc8da0d
user <user@DESKTOP-S6ON2JF>
2021-10-29 17:41:48 +0900

    a

--- 17_jdbc/src/ch02_update/InsertTest6.java
+++ 17_jdbc/src/ch02_update/InsertTest6.java
@@ -9,29 +9,39 @@ import java.util.Scanner;
 public class InsertTest6 {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
+		boolean run =true;
 		//1.드라이버로딩,2.연결객체 생성
 		try {
 			Connection con = DaoConnector.getInstance().getConnection();
 			//3.쿼리객체 생성
             Statement stmt = con.createStatement();
             //
+           String sql="";
+           while(run) {  
              System.out.println("등록할 고객명을 입력하세요>");
-             String name = scanner.nextLine();
+             String name = scanner.next();
              System.out.println("최초 입금액을 입력하세요>");
              int  balance = scanner.nextInt();
              
             //4.쿼리문 생성 
             //4-3.등록 처리
-            String sql="insert into customer ";//작성
+            sql="insert into account "
+  +" values((select '111'||'-'||lpad(to_number(nvl(substr(nvl(max(accountno),0),5,3),0))+1,3,'0') from account),'"
+  + name+"','"+balance+"')";//작성
             		//등록 실행
             		stmt.executeUpdate(sql);
+           //
+            System.out.println("계속할까요?(y/n)>");
+            String isRun = scanner.next();
+            if(isRun.equalsIgnoreCase("n")) run=false;
+           }		
             
             //5.결과 조회 
             sql ="select * from account order by accountNo";
             //조회 
            ResultSet rs = stmt.executeQuery(sql);
             while(rs.next()) {
-            System.out.println();//작성
+            System.out.println(rs.getString(1)+","+rs.getString(2)+","+rs.getString(3));//작성
             }
             //결과
             //-- 이름, 잔고 10000 입력
