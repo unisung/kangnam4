f3fb64b3b0d19379c489bd0d2f1c1d8153a51b94
user <user@DESKTOP-S6ON2JF>
2021-11-01 12:50:12 +0900

    a

--- 17_jdbc/src/ch06/CallableEx2.java
+++ 17_jdbc/src/ch06/CallableEx2.java
@@ -4,7 +4,7 @@ import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.DriverManager;
 
-public class CallableEx {
+public class CallableEx2 {
 	public static void main(String[] args) throws Exception {
 		//1.드라이버로딩
 		 Class.forName("oracle.jdbc.driver.OracleDriver");
@@ -16,15 +16,16 @@ public class CallableEx {
 		 Connection con = DriverManager.getConnection(url,userid,pwd);
 		 
 		 //3.callable객체 생성
-		 String sql="{call AVERAGEPRICEBYID(?)}";
+		 String sql="{call bookInsertORUpdate(?,?,?,?)}";//In 모드 매개변수 4개 등록
 		 CallableStatement cstmt = con.prepareCall(sql);
-		 //registerOutputParameter()--프로시져의 매개변수가 출력용(out 모드)인 경우 
-		 cstmt.registerOutParameter(1, java.sql.Types.NUMERIC);
-		 cstmt.setInt(1, 1);
+		 //입력파라미터 설정
+		 cstmt.setInt(1, 31);
+		 cstmt.setString(2, "야구가 좋냐?");
+		 cstmt.setString(3,"야구좋아출판사");
+		 cstmt.setInt(4, 30000);
 		 
 		 //4.실행
 		 cstmt.executeUpdate();
-		 System.out.println("평균:"+cstmt.getInt(1));
 		 
         //5.자원해제
 		 cstmt.close(); con.close();
