d8894d0593e3052fe8d0f2ca653b793eb6fb3b01
user <user@DESKTOP-S6ON2JF>
2021-11-01 16:04:37 +0900

    a

---
+++ 17_jdbc/src/ch09_savePoint/SavePointEx.java
@@ -0,0 +1,44 @@
+package ch09_savePoint;
+
+import java.sql.Connection;
+import java.sql.Savepoint;
+import java.sql.Statement;
+
+import ch02_update.DaoConnector;
+
+
+public class SavePointEx {
+	public static void main(String[] args) {
+		try {
+			//입력(transaction)처리
+			//1.드라이버로딩, 2.연결객체 생성하여 연결맺기
+			  Connection con = DaoConnector.getInstance().getConnection();
+			//3.sql문 전송객체 생성
+			  Statement stmt = con.createStatement();
+			  con.setAutoCommit(false);//수동처리
+			  con.commit();
+			//4.쿼리 전송 후 결과 처리
+			  String sql="insert into customer values((select max(custid)+1 from customer),'홍길이',null,null)"; 
+			  stmt.executeUpdate(sql);
+			 
+			  Savepoint s1=con.setSavepoint();
+			  
+			  sql="insert into customer values((select max(custid)+1 from customer),'또길동',null,null)"; 
+			  stmt.executeUpdate(sql);
+			
+			  Savepoint s2=con.setSavepoint();
+			  
+			   
+			  con.rollback(s1);
+			  
+			  
+			 con.commit();
+			  
+			//5.자원해제-생성된 객체 의 역순으로 해제
+			  stmt.close(); con.close();
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
